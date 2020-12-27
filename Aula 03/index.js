468db178dba913e9548450a9b34d4adda848c23c
manoel2020 <junior.miranda2100@gmail.com>
2020-12-27 17:37:49 -0400

    Tipos de passagem para a função console.log do JavaScript.

---
+++ Aula 03/index.js
@@ -0,0 +1,8 @@
+/*
+console.log('Funçao CONSOLE.LOG do Javascript');
+console.log("Funcção console.log com aspas duplas");
+console.log(`Console.log com CRASES`);
+*/
+
+console.log("Exercicio!")
+console.log('Meu Nome é "Manoel". estou aprendendo JavaScript as ',10,' da manhã.')
